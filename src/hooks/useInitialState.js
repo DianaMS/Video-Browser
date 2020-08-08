edbbb22bab401ae4042f976fe4e67258a74c34cb
DianaMS <dmartinezs@uni.pe>
2020-08-07 23:12:18 -0500

    contruyendo Hooks

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+const useInitialState = (api) => {
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
+
+  useEffect(() => {
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  return videos;
+};
+
+export default useInitialState;
